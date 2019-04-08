da5b66740391e6641cf3d5446625b36fce4ccc14
suvasishpradhan033@gmail.com <suvasishpradhan033@gmail.com>
2019-04-08 10:35:20 +0530

    twiiter project

--- followbot.js
+++ followbot.js
@@ -10,22 +10,22 @@ stream.on('follow', onFollowed)
 console.log("stream entered inside")
 function onFollowed(){
     console.log("follow event");
-    // var name= event.source.name;
-    // var screenName=event.source.screen_name;
-    // tweetIt('@'+screenName+'Thanks for following GONEBOT !');
+    var name= event.source.name;
+    var screenName=event.source.screen_name;
+    tweetIt('@'+screenName+'Thanks for following GONEBOT !');
 }
 
 
-// function tweetIt(txt){
+function tweetIt(txt){
 
-//     console.log(txt)
-//     var tweet={ status: txt };
-//     T.post('statuses/update',tweet , tweeted);
-//     function tweeted(err,data,response){
-//     if(err) "something went rong";
-//     else console.log("you tweeted who followed you.");
-//     }
-// }
+    console.log(txt)
+    var tweet={ status: txt };
+    T.post('statuses/update',tweet , tweeted);
+    function tweeted(err,data,response){
+    if(err) "something went rong";
+    else console.log("you tweeted who followed you.");
+    }
+}
 
 
 
